c9f12b17d98ef6725a868d54e5387cfb3ca25d82
samridhikhare <98250727+samridhikhare@users.noreply.github.com>
2022-01-31 16:02:51 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,5 @@
+#Write a python program which accepts the radius of a circle
+#from the user and computes area
+r=float(input("radius of the circle: "))
+area=3.1415926535*r*r
+print("area of the circle:",area)
