c9cd59ad81a571a0ef37925fc8b6a6a58f1c07be
samridhikhare <98250727+samridhikhare@users.noreply.github.com>
2022-01-31 16:16:21 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,10 @@
+# Python program to print positive Numbers in given range
+  
+start, end = -4,12
+  
+# iterating each number in list
+for num in range(start, end + 1):
+      
+    # checking condition
+    if num >= 1:
+        print(num, end = " ")
